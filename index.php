2923b746d0808e4776e196a47bf9ae82b9cb6f38
Latha Doddikadi <275.madhu@gmail.com>
2012-06-21 19:03:49 -0600

    changed index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo "Welcome to my app ";
+echo "Welcome to my app. This is Latha Doddikadi and I created this simple project in PHP. From here you can sign up, log in and add Questions. You can also add tags. Log in or Sign up now to check out how it works.";
 
 
 ?>
