ca8d7089ecd218142738288612bfaaf59065685b
Latha Doddikadi <275.madhu@gmail.com>
2012-06-21 18:05:09 -0600

    changed index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo "<html><title>My php project <title/><body> Welcome to my app </body></html>"
+echo "<html><title>My php project <title/><body> Welcome to my app </body></html>";
 
 
 ?>
